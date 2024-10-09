eff445b29cf93fc1125e1ce7c19e19caea074a45
ZDyIg <zignacio_21@alu.uabcs.mx>
2024-10-09 09:10:19 -0700

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "practica13.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
